b41e01e9d020d7f88972ee14103267c5100b5519
Derek Hanciles <drhanciles@icloud.com>
2018-11-27 15:10:18 -0700

    Data is properly imported and now being stored in state of my app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import kinderData from './data/kindergartners_in_full_day_program.js';
+import DistrictRepository from './helper.js'; 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      repostitory: new DistrictRepository(kinderData)
+    }
+  }
+
   render() {
     return (
       <div>Welcome To Headcount 2.0</div>
