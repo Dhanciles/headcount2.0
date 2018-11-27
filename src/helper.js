7db1539ec6fee7f9872ec5cc36f0dc3ec4167840
Derek Hanciles <drhanciles@icloud.com>
2018-11-26 17:44:11 -0700

    Iteration1-part1 is complete

--- src/helper.js
+++ src/helper.js
@@ -1,14 +1,30 @@
 export default class DistrictRepository {
   constructor(data) {
     this.stats = data.reduce((sortedDistricts, district) => {
+      let dataNumber; 
+      if (typeof district.Data === 'string') {
+        dataNumber = 0 
+      } else {
+        dataNumber = parseFloat(district.Data.toFixed(3))
+      }
       if (!sortedDistricts[district.Location]) {
-        sortedDistricts[district.Location] = {[district.TimeFrame]: district.Data}
+        sortedDistricts[district.Location.toUpperCase()] = {location: district.Location.toUpperCase(), stats: {[district.TimeFrame]: dataNumber}}
       } 
-      sortedDistricts[district.Location][district.TimeFrame] = district.Data 
+      sortedDistricts[district.Location.toUpperCase()].stats[district.TimeFrame] = dataNumber
       return sortedDistricts
     }, {})
   }
 
+  findByName = (name) => {
+    const districts = Object.keys(this.stats) 
+    if (!name) {
+      return undefined
+    } else {
+      const upperCase = name.toUpperCase(); 
+      const matchingDistrict = districts.find(district => district.includes(upperCase))
+      return this.stats[matchingDistrict]
+    }
 
+  }
 
 }
