379d159e00eb7270fe192e397031a9b1092fdc1d
Derek Hanciles <drhanciles@icloud.com>
2018-11-29 14:07:58 -0700

    Final commit on this branch

---
+++ src/Card.test.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import {shallow, mount} from 'enzyme'; 
+import Card from '.Card.js'; 
+
+describe('Card', () => {
+  beforeEach(() => {
+    const wrapper = shallow(<Card />)
+    test('should match the snapshot with all data passed in correctly')
+
+    const mockCard = {
+
+    }
+  })
+}); 
---
+++ src/CardContainer.test.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+import {shallow} from 'enzyme'; 
+import CardContainer from './CardContainer.js'; 
+
+describe('CardContainer', () => {
+  beforeEach(() => {
+    const wrapper = shallow(<CardContainer />)
+  }); 
+}); 
