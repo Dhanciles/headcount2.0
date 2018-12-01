7b6e8d5046c45edcc97a5e6dd534757d88b49527
Derek Hanciles <drhanciles@icloud.com>
2018-12-01 16:26:29 -0700

    New components for comparion card and container for cards being compared has been impelemented

---
+++ src/ComparisonCard.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import Proptypes from 'prop-types'; 
+
+const ComparisonCard = () => {
+  return (
+    <div className="comparison-card">
+      <article>
+        <h1>DISTRICT 1</h1> 
+        <h1>DISTRICT 2</h1>
+      </article>
+      <p>Returned Comparison</p>
+    </div>
+  )
+}
+export default ComparisonCard
---
+++ src/ComparisonContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import PropTypes from 'prop-types'; 
+import Card from './Card.js'; 
+import ComparisonCard from './ComparisonCard.js'; 
+
+const ComparisonContainer = () => {
+  return (
+    <div>
+      {<ComparisonCard}
+    </div>
+  )
+}
+export default ComparisonContainer
